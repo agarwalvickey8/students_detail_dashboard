1a31edba3267b222fb6bf76a85b8639bceee987f
Harish Jangra <harish0212@yahoo.com>
2024-03-16 15:46:33 +0530

    allowed hosts

--- studentsdata/settings.py
+++ studentsdata/settings.py
@@ -39,6 +39,7 @@ SECRET_KEY = 'django-insecure-j(o^4yf$py+v2)e7swja^@n9v6dlqm^4$d4e*_xbin$lcy&oas
 DEBUG = False
 
 ALLOWED_HOSTS = [
+    "localhost"
     "gcistudents.dv1.azure.schoollog.in",    
 ]
 
